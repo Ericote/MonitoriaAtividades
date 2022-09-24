ac596bef281868b7ea98542562b77fe21595caac
Ericote <ericvinicius848@gmail.com>
2022-09-24 00:19:58 -0300

    teste

---
+++ Teste.java
@@ -0,0 +1,14 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("EXERCICIO 1");
+
+            int nota1 = 10;
+            int nota2 = 10;
+            int nota3 = 10;
+
+            int media = (nota1 + nota2 + nota3) / 3;
+
+            System.out.println(media);
+    }
+    
+}
